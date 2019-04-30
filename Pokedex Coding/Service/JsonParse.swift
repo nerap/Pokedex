31e05a1cf18f11b854a7cf06503cab956c6b9792
thierry <thierrypoupon@hotmail.com>
2019-04-30 15:06:31 +0200

    Mon super commit :) :) :) :)

---
+++ Pokedex Coding/Service/JsonParse.swift
@@ -0,0 +1,15 @@
+
+//let json = '{"_id":"557a723880a20c9db3bc31c2", "pkdx_id":1, "national_id":1, "name":"Bulbasaur"}';
+
+
+class bindToPokemon(json)-> Pokemon {
+    return
+    
+    
+}
+
+
+
+
+
+
